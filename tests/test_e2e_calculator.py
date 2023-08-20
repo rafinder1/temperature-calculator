bba177b3f0893649e5077c2c913d0dbef964e3cf
rafinder1 <rafpro33@gmail.com>
2023-08-20 22:10:45 +0200

    adding main information about heat

--- tests/test_e2e_calculator.py
+++ tests/test_e2e_calculator.py
@@ -10,7 +10,8 @@ class MainTestCase(unittest.TestCase):
     def test_temperature_calculator(self):
         expected = pd.DataFrame
         data_building_partition = FileHelper.read_csv("tests/building_partition.txt")
-        heat_information = dict()
-        print(data_building_partition)
+
+        heat_information = FileHelper.read_json("tests/heat_information.json")
+
         data_frame = TempCalculator.calculate(data_building_partition, heat_information)
         self.assertEqual(expected, data_frame)
--- tools/file_helper.py
+++ tools/file_helper.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 
 
 class FileHelper:
@@ -6,3 +7,8 @@ class FileHelper:
     def read_csv(file):
         # Read the text file into a DataFrame
         return pd.read_csv(file, sep=",", header=0)
+
+    @staticmethod
+    def read_json(file_path):
+        with open(file_path, "r") as json_file:
+            return json.load(json_file)
