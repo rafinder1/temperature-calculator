0a77b383af351b3ef63b89afccfe5f2aaa2a67c7
rafinder1 <rafpro33@gmail.com>
2023-09-16 15:38:51 +0200

    add test to mulit variants calc - polystyrene

--- calculator/multivariants/multivariants.py
+++ calculator/multivariants/multivariants.py
@@ -1,10 +1,34 @@
 from pandas import DataFrame
+import numpy as np
 from calculator.basic.config import MethodName
+from calculator.basic.calculator import TempCalculator
+from calculator.multivariants.config import Rsi, Rse
 
 
 class MultiVariantsCalculator:
     @staticmethod
     def change_polystyrene(data_building_partition: DataFrame, heat_information: dict, polystyrene_data: DataFrame,
                            method: MethodName) -> DataFrame:
-        print(polystyrene_data)
-        return DataFrame
+        index_polystyrene = data_building_partition[data_building_partition['type_layer'] == 'ocieplenie'].index
+
+        for number_row, polystyrene_param in polystyrene_data.iterrows():
+            data_building_partition.loc[index_polystyrene, 'name_layer'] = polystyrene_param['name_layer']
+            data_building_partition.loc[index_polystyrene, 'thickness'] = polystyrene_param['thickness']
+            data_building_partition.loc[index_polystyrene, 'thermal_conductivity'] = polystyrene_param[
+                'thermal_conductivity']
+
+            U = 1 / (Rsi + Rse + sum(
+                (data_building_partition['thickness']) / data_building_partition['thermal_conductivity']))
+
+            temperatures_all_layers = TempCalculator.calculate(data_building_partition, heat_information, method)
+
+            temperature_last_layer = temperatures_all_layers.iloc[-1]['temperatures']
+
+            polystyrene_data.loc[number_row, 'temperatures'] = temperature_last_layer
+
+            if U > 0.2:
+                polystyrene_data.loc[number_row, 'comments'] = 'U>0.2'
+            if temperature_last_layer > 30:
+                polystyrene_data.loc[number_row, 'comments'] = 'temperature above 30'
+
+        return polystyrene_data
--- tests/multivariants/test_e2e_multivariants.py
+++ tests/multivariants/test_e2e_multivariants.py
@@ -1,12 +1,14 @@
 import unittest
-
+from pandas.testing import assert_frame_equal
 from calculator.multivariants.multivariants import MultiVariantsCalculator
 from calculator.basic.config import MethodName
 from tools.file_helper import FileHelper
 
 
 class MainTestCase(unittest.TestCase):
-    def test_temperature_calculator_dirichlet_neumann(self):
+    def test_multi_variants_calculator(self):
+        expected = FileHelper.read_csv("tests/multivariants/expected_temperatures.csv")
+
         data_building_partition = FileHelper.read_csv("tests/multivariants/building_partition.txt")
 
         heat_information = FileHelper.read_json("tests/multivariants/heat_information_dirichlet_neumann.json")
@@ -19,8 +21,5 @@ class MainTestCase(unittest.TestCase):
         output = MultiVariantsCalculator.change_polystyrene(data_building_partition=data_building_partition,
                                                             heat_information=heat_information,
                                                             polystyrene_data=polystyrene_data, method=method)
-        self.assertTrue(True)
-        # expected = data_building_partition.copy(deep=True)
-        # expected['temperatures'] = [-19.60, -7.60, 20.97, 21.12]
-        #
-        # self.assertTrue(expected.equals(output))
+
+        assert_frame_equal(expected, output)
