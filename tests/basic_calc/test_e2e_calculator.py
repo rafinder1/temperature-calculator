c2fc54269b8120b99683405b1f058516eedf5163
rafinder1 <rafpro33@gmail.com>
2023-10-16 19:51:06 +0200

    delete filehelper

--- tests/basic_calc/test_e2e_calculator.py
+++ tests/basic_calc/test_e2e_calculator.py
@@ -2,13 +2,11 @@ import unittest
 
 from calculator.basic.calculator import TempCalculator
 from calculator.shared_data.outside_inside_thermal_data import OutsideInsideThermalData
-from tools.file_helper import FileHelper
+from tests.multivariants.building_partition import data_building_partition
 
 
 class MainTestCase(unittest.TestCase):
     def test_temperature_calculator_dirichlet_neumann(self):
-        data_building_partition = FileHelper.read_csv("tests/basic_calc/building_partition.txt")
-
         outside_inside_thermal_data = OutsideInsideThermalData(
             OUTSIDE_TEMPERATURE=-20,
             INSIDE_HEATER_POWER=80)
@@ -24,8 +22,6 @@ class MainTestCase(unittest.TestCase):
         self.assertTrue(expected.equals(output))
 
     def test_temperature_calculator_dirichlet_dirichlet(self):
-        data_building_partition = FileHelper.read_csv("tests/basic_calc/building_partition.txt")
-
         outside_inside_thermal_data = OutsideInsideThermalData(
             OUTSIDE_TEMPERATURE=-20,
             INSIDE_TEMPERATURE=20
@@ -33,7 +29,8 @@ class MainTestCase(unittest.TestCase):
 
         method = 'finite_element_method'
 
-        output = TempCalculator.calculate(data_building_partition, outside_inside_thermal_data, method)
+        output = TempCalculator.calculate(data_building_partition, outside_inside_thermal_data,
+                                          method)
 
         expected = data_building_partition.copy(deep=True)
         expected['temperatures'] = [-19.61, -7.94, 19.85, 20.00]
--- tools/file_helper.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import json
-
-
-class FileHelper:
-    @staticmethod
-    def read_csv(file):
-        return pd.read_csv(file, sep=",", header=0)
