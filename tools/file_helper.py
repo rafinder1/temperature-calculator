5d3a4a06f27af55f4f972bd5e152363f54c6efa4
rafinder1 <rafpro33@gmail.com>
2023-10-16 19:47:24 +0200

    WIP - delete FileHelper

--- tools/file_helper.py
+++ tools/file_helper.py
@@ -6,12 +6,3 @@ class FileHelper:
     @staticmethod
     def read_csv(file):
         return pd.read_csv(file, sep=",", header=0)
-
-    @staticmethod
-    def read_json(file_path):
-        with open(file_path, "r") as json_file:
-            return json.load(json_file)
-
-    @staticmethod
-    def read_excel(file, sheet_name):
-        return pd.read_excel(file, sheet_name=sheet_name)
